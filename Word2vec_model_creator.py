dad3f05cc77d07df3e2e38800114cd4181a57422
Rabia Yasa Kostas <35491090+rabiayasa@users.noreply.github.com>
2019-07-09 22:22:17 +0100

    Add files via upload

---
+++ Word2vec_model_creator.py
@@ -0,0 +1,28 @@
+from gensim.models.word2vec import LineSentence
+from gensim.models import Word2Vec
+import warnings
+import time
+warnings.filterwarnings("ignore")
+
+#size, window and cbow or skipgram defination
+dimension=[600,1000]
+window=[4,10]
+objective_function=[0,1]
+
+#creating a table for outputs on screen
+of_dict = {1: "skipgram",0: "cbow"}
+input_file="all_text.txt"
+print ('%-25s %-25s' % ("file name","time in seconds"))
+
+
+#model creation
+for i in dimension:
+    for ii in window:
+        for iii in objective_function:  
+            second=time.time()#time stamp
+            model = Word2Vec(LineSentence(input_file), size=i, window=ii,sg=iii)
+            #writing an output file with the name of size, window and cbow or skipgram
+            output_file="model_"+str(i)+"_"+str(ii)+"_"+of_dict[iii]
+            model.save(output_file)
+            model.wv.save_word2vec_format(output_file, binary=False)
+            print ('%-25s %-25s' % (output_file,str(float(time.time()-second))))
